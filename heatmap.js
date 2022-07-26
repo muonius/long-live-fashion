2735165c6bc1b9ea895e4ea19b8e38c3dd45f38a
Yang Zhao <freda.y.zhao@gmail.com>
2022-07-25 22:31:37 -0400

    update

--- heatmap.js
+++ heatmap.js
@@ -1,11 +1,12 @@
 async function drawHeatmap() {
   //*******1. Access Data */
-  const data = await d3.csv("./assets/summary.csv");
-  //   console.log(data);
-  const column = Object.keys(data[0]);
+  const data = await d3.json("./assets/test.csv");
+  console.log(data);
+  const dateParser = d3.timeParse("%Y");
+
   const row = data.map((d) => d["Designer"]);
   //   console.log(column);
-  //   console.log(row);
+  // console.log(row);
   const xAccessor = (d) => d["Designer"];
   //   console.log(xAccessor(data[0]));
 }
